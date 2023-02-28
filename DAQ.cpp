727b7707e799f66254c8676f492ccfe5f399c40e
Thomas Ales <alestk@gmail.com>
2023-02-27 19:47:25 -0600

    added errno -> error message function

--- DAQ.cpp
+++ DAQ.cpp
@@ -20,3 +20,22 @@ void DAQ::StartClock()
 	// TODO: Write function to spawn thread and wait
 	//       for condition variable to stop the clock
 }
+
+void DAQ::StopClock()
+{
+	// TODO: Trigger condition variable to shutdown
+	//       the clock task.
+}
+
+// Takes the cryptic error code and returns a slightly less cryptic 
+// error message from the National Instruments DAQmx library.
+std::string LookupDAQError(int32 _errorCode)
+{
+	// get the required buffer size
+	int32 bufSize = DAQmxGetErrorString(_errorCode, NULL, 0);
+	// get the error text and convert to a string and return
+	// the string
+	char* errorMsg = new char[bufSize];
+	DAQmxGetErrorString(_errorCode, errorMsg, bufSize);
+	return std::string(errorMsg);
+}
--- DAQ.h
+++ DAQ.h
@@ -19,5 +19,21 @@ public:
 	void ConfigureClock(float _frequency, float _duty = 0.50);
 	void StartClock();
 	void StopClock();
+	std::string LookupDAQError(int32 _errorCode);
+
+	// These parameters are for configuring the
+	// 'counter' output channel. This counter output
+	// will generate the trigger signal for the cameras.
+	std::string OutputPort;
+	int32 ClockUnits = DAQmx_Val_Hz;
+	int32 IdleState = DAQmx_Val_Low;
+	float64 InitialDelay = 0.0;
+	float64 Frequency = 0.0;
+	float64 DutyCycle = 0.0;
+	// These are some 'implicit timing' paramaters
+	// that need to be set in order to use the counter.
+	int32 SampleMode = DAQmx_Val_ContSamps;
+	uInt64 BufferSize = 250;
+
 };
 
